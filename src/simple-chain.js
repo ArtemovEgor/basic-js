5fa54d0bd60b95783d1fafaa329c5d6f2ea06306
Egor Artemov <mercury2juice@gmail.com>
2025-11-11 03:08:10 +0400

    feat: add simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,27 @@ const { NotImplementedError } = require('../lib');
  *
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  addLink( value ) {
+    this.chain.push(`( ${String(value)} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  removeLink( position ) {
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    const finishedChain = this.chain.join("~~");
+    this.chain = [];
+    return finishedChain;
   },
 };
 
